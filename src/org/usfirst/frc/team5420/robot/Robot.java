37186386b2a4a2a64ccc02ad78712d79d7cc8d47
Noah Halstead <nhalstead00@gmail.com>
2017-07-27 16:12:31 -0400

    Added the ArcadeDrive
    Added the code for the ArcadeDrve control.

--- src/org/usfirst/frc/team5420/robot/Robot.java
+++ src/org/usfirst/frc/team5420/robot/Robot.java
@@ -89,6 +89,7 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void teleopPeriodic() {
 		//Drive Motor Control
+		myRobot.arcadeDrive(joystick0.getRawAxis(1), joystick0.getRawAxis(4)); //Pulling joystick values from Only One Controller not One from CTRL1 and the other from CTRL2
 		
 		//Get the Upper and and the Lower Status of the Sensor and Allow or Disallow Operation(s) control.
 		//Look at "the axis or button number as a parameter and return the corresponding value" https://wpilib.screenstepslive.com/s/4485/m/13809/l/599723-joysticks
